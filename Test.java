5d7da1540bb362f6c81f851a6c31cb9a8bcaf92b
20nan <02nan093may11@gmail.com>
2024-12-23 01:17:29 +0900

    2Fixed a bug in Test.java

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 public class Test {
     public static void main(String[] args) {
         // 数値のリストを作成
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12);
 
         // 偶数をフィルタリングして新しいリストを作成
         List<Integer> evenNumbers = numbers.stream()
